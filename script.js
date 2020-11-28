730f9bfa1768ec23fa0c041d2e1c7c4c18fe9d85
AB <johndoe@example.com>
2020-11-28 12:36:47 -0500

    Included active image state.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const carouselImages = document.querySelector('.carousel__images');
+const images = document.querySelectorAll('.carousel__images img');
 const carouselButtons = document.querySelectorAll('.carousel__button');
 const numberOfImages = document.querySelectorAll('.carousel__images img').length;
 let imageIndex = 1;
@@ -9,15 +10,22 @@ carouselButtons.forEach(button => {
     if (event.target.id === 'previous') {
       if (imageIndex !== 1) {
         imageIndex--;
-        translateX += 300;
+        translateX += 250;
       }
     } else {
       if (imageIndex !== numberOfImages) {
         imageIndex++;
-        translateX -= 300;
+        translateX -= 250;
       }
     }
-    
+
     carouselImages.style.transform = `translateX(${translateX}px)`;
+    images.forEach((image, index) => {
+      if (index === imageIndex - 1) {
+        image.classList.add('active');
+      } else {
+        image.classList.remove('active');
+      }
+    });
   });
 });
